89614ee2737d22e172a5b7f6639067e1b4368bf5
cyberpink <32724299+cyberpink@users.noreply.github.com>
2019-12-09 11:08:33 -0600

    squish

--- 2019/day9/main.ml
+++ 2019/day9/main.ml
@@ -1,17 +1,12 @@
-type req =
-  | Get of (int -> req)
-  | Put of int * (unit -> req)
-  | Done
+let do_get k i o = (k i) i o
+let do_put x k i o = (k ()) i (x :: o)
+let do_done i o = List.rev o
 
 let run mem =
   let rec go pc rb =
     let _op = mem.(pc) in
     let opcode = _op mod 100 in
-    let modes =
-      [| (_op / 100) mod 10;
-         (_op / 1000) mod 10;
-         (_op / 10000) mod 10 |]
-    in
+    let modes = [| _op / 100 mod 10; _op / 1000 mod 10; _op / 10000 mod 10 |] in
     let o i = match modes.(i-1) with
       | 0 -> mem.(pc + i)
       | 1 -> pc + i
@@ -23,23 +18,20 @@ let run mem =
     match opcode with
     | 1 -> set 3 (get 1 + get 2); go (pc + 4) rb
     | 2 -> set 3 (get 1 * get 2); go (pc + 4) rb
-    | 3 -> Get (fun v -> set 1 v; go (pc + 2) rb)
-    | 4 -> Put (get 1, fun () -> go (pc + 2) rb)
+    | 3 -> do_get (fun v -> set 1 v; go (pc + 2) rb)
+    | 4 -> do_put (get 1) (fun () -> go (pc + 2) rb)
     | 5 -> if get 1 != 0 then go (get 2) rb else go (pc + 3) rb
     | 6 -> if get 1 == 0 then go (get 2) rb else go (pc + 3) rb
     | 7 -> set 3 (if get 1 < get 2 then 1 else 0); go (pc + 4) rb
     | 8 -> set 3 (if get 1 == get 2 then 1 else 0); go (pc + 4) rb
     | 9 -> go (pc+2) (rb + get 1)
-    | 99 -> Done
+    | 99 -> do_done
     | _ -> failwith "bad op"
   in go 0 0
 
-let run_program p input =
-  let rec handle o = function
-    | Done -> print_endline @@ String.concat "," @@ List.map string_of_int @@ List.rev o
-    | Get k -> handle o @@ k input
-    | Put (x, k) -> handle (x :: o) @@ k ()
-  in handle [] @@ run p
+let run_program p input = 
+  print_endline @@ String.concat "," @@ List.map string_of_int @@
+  (run p) input []
 
 let rec init =
   let p = List.map int_of_string @@ String.split_on_char ',' @@ read_line () in
