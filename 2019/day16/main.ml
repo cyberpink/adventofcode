e11617bf30a57f45475e2099fd497c4e9d591ab6
cyberpink <32724299+cyberpink@users.noreply.github.com>
2019-12-16 13:59:28 -0600

    ..

--- 2019/day16/main.ml
+++ 2019/day16/main.ml
@@ -1,22 +1,23 @@
 let code = [| 0;1;0;-1 |]
-let run input p =
-  let out = ref 0 in
-  for i = 0 to Array.length input - 1 do
-    out:= !out + code.((i+1)/(p+1) mod 4) * input.(i)
-  done;
-  abs @@ !out mod 10
-
-let main =
-  let str = ref "" in
-  Scanf.scanf "%s\n" (fun i -> str := i);
-  let input = Array.make (String.length !str) 0 in
-  String.iteri (fun i c -> input.(i) <- (Char.code c) - (Char.code '0')) !str;
+let p1 _input =
+  let input = Array.copy _input in
   let len = Array.length input in
   let output = Array.make len 0 in
-  for r = 1 to 100 do
-    for i = 0 to len - 1 do
-      output.(i) <- run input i
+  for iter = 1 to 100 do
+    for d = 0 to len - 1 do
+      let row = ref 0 in
+      for i = 0 to len - 1 do
+        row := !row + code.((i+1)/(d+1) mod 4) * input.(i)
+      done;
+      output.(d) <- abs @@ !row mod 10
     done;
     Array.iteri (fun i x -> input.(i) <- x) output
   done;
-  print_endline @@ String.concat "" @@ List.map string_of_int @@ Array.to_list @@ Array.sub output 0 8
+  Array.sub output 0 8  
+
+let main =
+  let str = Scanf.scanf "%s\n" (fun i -> i) in
+  let input = Array.make (String.length str) 0 in
+  String.iteri (fun i c -> input.(i) <- (Char.code c) - (Char.code '0')) str;
+  print_endline @@ String.concat "" @@
+  List.map string_of_int @@ Array.to_list @@ p1 input
