2427a3c56dfb61145c30a699672f06115605ef80
cyberpink <32724299+cyberpink@users.noreply.github.com>
2019-12-16 15:48:38 -0600

    ...

--- 2019/day16/main.ml
+++ 2019/day16/main.ml
@@ -5,19 +5,21 @@ let p1 _input =
   let output = Array.make len 0 in
   for iter = 1 to 100 do
     for d = 0 to len - 1 do
-      let row = ref 0 in
+      output.(d) <- 0;
       for i = 0 to len - 1 do
-        row := !row + code.((i+1)/(d+1) mod 4) * input.(i)
+        output.(d) <- output.(d) + code.((i+1)/(d+1) mod 4) * input.(i)
       done;
-      output.(d) <- abs @@ !row mod 10
+      output.(d) <- abs @@ output.(d) mod 10
     done;
     Array.iteri (fun i x -> input.(i) <- x) output
   done;
-  Array.sub output 0 8  
+  Array.sub output 0 8
 
 let main =
   let str = Scanf.scanf "%s\n" (fun i -> i) in
   let input = Array.make (String.length str) 0 in
   String.iteri (fun i c -> input.(i) <- (Char.code c) - (Char.code '0')) str;
   print_endline @@ String.concat "" @@
-  List.map string_of_int @@ Array.to_list @@ p1 input
+  List.map string_of_int @@ Array.to_list @@ p1 input;
+  Printf.printf "%d\n" ((String.length str * 10000) - (int_of_string (String.sub str 0 7)))
+    
